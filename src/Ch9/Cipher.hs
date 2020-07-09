4d867953ee41f6fd400afb0c6cd2fde4cd0b08a0
Vincibean <andrebessi00@gmail.com>
2020-07-09 20:02:07 +0100

    improved symmetry of cipher

--- src/Ch9/Cipher.hs
+++ src/Ch9/Cipher.hs
@@ -2,7 +2,6 @@ module Ch9.Cipher (caesar, unCaesar) where
 
 import           Data.Char
 
-
 minOrdUpper :: Int
 minOrdUpper = ord 'A'
 
@@ -21,5 +20,5 @@ unCaesar n = map $ shift (-n)
 shift n c
   | isLower c = shiftMod minOrdLower c
   | isUpper c = shiftMod minOrdUpper c
-  | otherwise = ' ' -- c would probably be better here
+  | otherwise = c
   where shiftMod m = chr . (+ m) . (`mod` alphSize) . (+ n) . (subtract m) . ord
